953287518d4332c2de66de669958900cd31b7df2
joachimbbp <104856283+joachimbbp@users.noreply.github.com>
2024-10-01 17:43:53 -0400

    Blender trasnform affine hack implemented

--- src/blender_scripts/anatomy_vdb_from_np.py
+++ src/blender_scripts/anatomy_vdb_from_np.py
@@ -20,6 +20,7 @@ anatomy_volume = np.load(anat_path)
 affine = np.load(affine_path)
 scale = (affine[0][0], affine[1][1], affine[2][2])
 print("scale", scale)
+print(affine)
 
 grid = openvdb.FloatGrid() #FloatGrid supported without crazy build times
 #grid.transform = openvdb.createLinearTransform(affine)
@@ -28,7 +29,12 @@ grid.gridClass = openvdb.GridClass.UNKNOWN
 grid.name='anatomy'
 
 openvdb.write(output_path,grid)
-bpy.ops.object.volume_import(filepath=output_path, files=[], scale=(0.0,0.0,20.0), rotation=(0.0,0.0,3.0))
+bpy.ops.object.volume_import(filepath=output_path, files=[])
 
 #SO, our afine is nicely formatted as per openvdb's specifications, however the linear transform does not work. Ugh.
 #As a solve, we COULD dial this in as volume transformations in blender. NOT GREAT but it should work
+
+#TODO eliminate unessesary vars here
+bpy.ops.transform.resize(value=(affine[0][0], affine[1][1], affine[2][2]), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(False, False, True), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, snap=False, snap_elements={'INCREMENT'}, use_snap_project=False, snap_target='CLOSEST', use_snap_self=True, use_snap_edit=True, use_snap_nonedit=True, use_snap_selectable=False, release_confirm=True)
+
+bpy.ops.transform.translate(value=(affine[3][0], affine[3][1], affine[3][2]), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(False, True, False), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, snap=False, snap_elements={'INCREMENT'}, use_snap_project=False, snap_target='CLOSEST', use_snap_self=True, use_snap_edit=True, use_snap_nonedit=True, use_snap_selectable=False, release_confirm=True)
