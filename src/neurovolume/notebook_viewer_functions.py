0c2c77e22605e05a10cd7d8845799e70756eb7c9
joachimbbp <104856283+joachimbbp@users.noreply.github.com>
2024-11-07 11:35:37 -0500

    match statements used for dimensions on explore_3d_vol

--- src/neurovolume/notebook_viewer_functions.py
+++ src/neurovolume/notebook_viewer_functions.py
@@ -2,27 +2,34 @@ import matplotlib.pyplot as plt
 from ipywidgets import interact
 import numpy as np
 
+
 default_cmap = 'nipy_spectral'
 default_figsize = (4,4)
+empty_mask = np.empty((1,1,1))
 
-def explore_3D_vol(vol: np.ndarray, cmap=default_cmap, dim="x"):
+def explore_3D_vol(vol: np.ndarray, cmap=default_cmap, dim="x", mask=empty_mask):
     #TODO rename dims to saggital, etc
     #TODO shot all 3D at the same time with https://matplotlib.org/stable/users/explain/axes/axes_intro.html
+    masking = False
+    if mask != empty_mask:
+        masking = True
 
     def x_coord(slice):
         plt.figure(figsize=default_figsize)
         plt.imshow(vol[slice, :, :], cmap=default_cmap)
-    if dim=="x":
-        interact(x_coord, slice=(0, vol.shape[0]-1))
 
     def y_coord(slice):
         plt.figure(figsize=default_figsize)
         plt.imshow(vol[:, slice, :], cmap=default_cmap)
-    if dim=="y":
-        interact(y_coord, slice=(0, vol.shape[1]-1))
     
     def z_coord(slice):
         plt.figure(figsize=default_figsize)
         plt.imshow(vol[:, :, slice], cmap=default_cmap)
-    if dim=="z":
-        interact(z_coord, slice=(0, vol.shape[2]-1))
+
+    match dim:
+        case "x":
+            interact(x_coord, slice=(0, vol.shape[0]-1))
+        case "y":
+            interact(y_coord, slice=(0, vol.shape[1]-1))
+        case "z":
+            interact(z_coord, slice=(0, vol.shape[2]-1))
