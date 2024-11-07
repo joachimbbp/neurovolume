61a7d0bb731b70af2ad29a49297a7a780af19088
joachimbbp <104856283+joachimbbp@users.noreply.github.com>
2024-11-07 12:38:00 -0500

    masking implemented for all three dimensions

--- src/neurovolume/notebook_viewer_functions.py
+++ src/neurovolume/notebook_viewer_functions.py
@@ -2,29 +2,41 @@ import matplotlib.pyplot as plt
 from ipywidgets import interact
 import numpy as np
 
-
+#Visualization options (feel free to tweak):
 default_cmap = 'nipy_spectral'
 default_figsize = (4,4)
+mask_contor_color = 'white'
+mask_contor_thickness = 1
+mask_contor_levels = [0.5]
+
+#control flow dependent stuff (don't change these):
 empty_mask = np.empty((1,1,1))
 
 def explore_3D_vol(vol: np.ndarray, cmap=default_cmap, dim="x", mask=empty_mask):
-    #TODO rename dims to saggital, etc
+    #TODO rename dims to saggital, etc, or at least print that on the plot
     #TODO shot all 3D at the same time with https://matplotlib.org/stable/users/explain/axes/axes_intro.html
     masking = False
-    if mask != empty_mask:
+    if mask.all() != empty_mask.all():
         masking = True
 
+    #TODO dry if possible
     def x_coord(slice):
         plt.figure(figsize=default_figsize)
         plt.imshow(vol[slice, :, :], cmap=default_cmap)
+        if masking:
+            plt.contour(mask[slice,:,:], levels=mask_contor_levels, colors=mask_contor_color, linewidths=mask_contor_thickness)
 
     def y_coord(slice):
         plt.figure(figsize=default_figsize)
         plt.imshow(vol[:, slice, :], cmap=default_cmap)
+        if masking:
+            plt.contour(mask[:,slice,:], levels=mask_contor_levels, colors=mask_contor_color, linewidths=mask_contor_thickness)
     
     def z_coord(slice):
         plt.figure(figsize=default_figsize)
         plt.imshow(vol[:, :, slice], cmap=default_cmap)
+        if masking:
+            plt.contour(mask[:,:,slice], levels=mask_contor_levels, colors=mask_contor_color, linewidths=mask_contor_thickness)
 
     match dim:
         case "x":
