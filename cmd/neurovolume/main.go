53e345ac7253e3cdf4e0126848019f531c4ed904
joachimbbp <104856283+joachimbbp@users.noreply.github.com>
2025-04-04 16:38:12 -0400

    main moved back to normal control flow

--- cmd/neurovolume/main.go
+++ cmd/neurovolume/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/joachimbbp/neurovolume/pkg/open"
-	"github.com/joachimbbp/neurovolume/pkg/utils"
 	"github.com/joachimbbp/neurovolume/pkg/vdb"
-	"github.com/joachimbbp/neurovolume/pkg/volume"
 )
 
 func main() {
@@ -19,36 +20,37 @@ func main() {
 
 	*/
 
+	niftiPath := os.Args[1]
+	vol := open.NIfTI1(niftiPath)
+	vol.NormalizeVolume(true)
+
+	outputFolder := os.Args[2]
+	vol.SaveMetadata(outputFolder)
+	vdb.WriteFromVolume(&vol, outputFolder, "")
+	fmt.Println("Read in Volume:")
+	vol.PrintVolumeInfo()
+
+	//METHOD OF SUBTRACTION, hard coded example:
 	/*
-		niftiPath := os.Args[1]
-		vol := open.NIfTI1(niftiPath)
-		fmt.Println(vol.BaseName, " loaded, shape: ", vol.Shape)
-		vol.NormalizeVolume()
-		fmt.Println("Normalized: ", vol.Normalized)
+		outputFolder := "/Users/joachimpfefferkorn/repos/neurovolume/output"
+		utils.ClearOutputFolder(outputFolder)
 
-		outputFolder := os.Args[2]
-		vol.SaveMetadata(outputFolder)
-		vdb.WriteFromVolume(&vol, outputFolder, "")
+		experimental := open.NIfTI1("/Users/joachimpfefferkorn/repos/neurovolume/media/sub-01_task-emotionalfaces_run-1_bold.nii.gz")
+		experimental.NormalizeVolume(true)
+		control := open.NIfTI1("/Users/joachimpfefferkorn/repos/neurovolume/media/sub-01_task-rest_bold.nii.gz")
+		control.NormalizeVolume(true)
+
+		result := volume.SubtractAndClip(experimental, control)
+		result.NormalizeVolume(true)
+
+		result.SaveMetadata(outputFolder)
+		vdb.WriteFromVolume(&result, outputFolder, "")
+		experimental.SaveMetadata(outputFolder)
+		vdb.WriteFromVolume(&experimental, outputFolder, "")
+		control.SaveMetadata(outputFolder)
+		vdb.WriteFromVolume(&control, outputFolder, "")
+
+		result.SetMean() //for debugging only
+		result.PrintVolumeInfo()
 	*/
-
-	outputFolder := "/Users/joachimpfefferkorn/repos/neurovolume/output"
-	utils.ClearOutputFolder(outputFolder)
-
-	experimental := open.NIfTI1("/Users/joachimpfefferkorn/repos/neurovolume/media/sub-01_task-emotionalfaces_run-1_bold.nii.gz")
-	experimental.NormalizeVolume(true)
-	control := open.NIfTI1("/Users/joachimpfefferkorn/repos/neurovolume/media/sub-01_task-rest_bold.nii.gz")
-	control.NormalizeVolume(true)
-
-	result := volume.Subtract(experimental, control)
-	result.NormalizeVolume(true)
-
-	result.SaveMetadata(outputFolder)
-	vdb.WriteFromVolume(&result, outputFolder, "")
-	experimental.SaveMetadata(outputFolder)
-	vdb.WriteFromVolume(&experimental, outputFolder, "")
-	control.SaveMetadata(outputFolder)
-	vdb.WriteFromVolume(&control, outputFolder, "")
-
-	result.SetMean() //for debugging only
-	result.PrintVolumeInfo()
 }
