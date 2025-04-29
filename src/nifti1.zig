2513536d420ea9e7482cc836ca63afaba8cc9547
joachimbbp <104856283+joachimbbp@users.noreply.github.com>
2025-04-29 16:04:04 -0400

    zig reads header

---
+++ src/nifti1.zig
@@ -0,0 +1,74 @@
+const std = @import("std");
+
+//Nifit1 Header
+pub const Header = extern struct {
+    sizeofHdr: i32, //Must be 348
+    dataType: [10]u8,
+    dbName: [18]u8,
+    extents: i32,
+    sessionError: i16,
+    regular: u8,
+    dimInfo: u8, //key for MRI slice ordering
+
+    dim: [8]i16,
+    intentP1: f32,
+    intentP2: f32,
+    intentP3: f32,
+    intentCode: i16,
+    datatype: i16,
+    bitpix: i16,
+    sliceStart: i16,
+    pixdim: [8]f32,
+    voxOffset: f32,
+    sclSlope: f32,
+    sclInter: f32,
+    sliceEnd: i16,
+    sliceCode: u8,
+    xyztUnits: u8,
+    calMax: f32,
+    calMin: f32,
+    sliceDuration: f32,
+    toffset: f32,
+    glmax: i32,
+    glmin: i32,
+
+    descrip: [80]u8,
+    auxFile: [24]u8,
+
+    qformCode: i16,
+    sformCode: i16,
+    quaternB: f32,
+    quaternC: f32,
+    quaternD: f32,
+    qoffsetX: f32,
+    qoffsetY: f32,
+    qoffsetZ: f32,
+
+    srowX: [4]f32,
+    srowY: [4]f32,
+    srowZ: [4]f32,
+
+    intentName: [16]u8,
+
+    magic: [4]u8,
+};
+
+pub const NIfTI1Image = extern struct {
+    // data: []const u8,
+    dimensions: [4]usize,
+};
+
+test "open" {
+    // const static = "/Users/joachimpfefferkorn/repos/neurovolume/media/sub-01_T1w.nii";
+    const bold = "/Users/joachimpfefferkorn/repos/neurovolume/media/sub-01_task-emotionalfaces_run-1_bold";
+    const file = try std.fs.cwd().openFile(bold, .{});
+    defer file.close();
+    const reader = file.reader();
+    const header = try reader.readStruct(Header);
+    std.debug.print("{any}", .{header});
+}
+
+//convention:
+//Types: pascal
+//fields: snake
+//methods: camel
