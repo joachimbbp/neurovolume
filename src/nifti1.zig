4c28b064c2e0de6e4c85ddd0ab2732fcb0afae55
joachimbbp <104856283+joachimbbp@users.noreply.github.com>
2025-05-01 19:06:19 -0400

    this test passes

--- src/nifti1.zig
+++ src/nifti1.zig
@@ -52,34 +52,39 @@ pub const Header = extern struct {
     magic: [4]u8,
 };
 
-pub const Image = extern struct {
+pub const Image = struct { //not sure why this was originally an extern struct?
     header: *const Header,
-    data: *const []u8,
+    data: []u8,
+    allocator: *const std.mem.Allocator,
 
     pub fn printHeader(self: *const Image) void {
         std.debug.print("{any}", .{self.header});
     }
     pub fn init(filepath: []const u8) !Image {
+        const allocator = &std.heap.page_allocator;
         //Load File
         const file = try std.fs.cwd().openFile(filepath, .{});
         defer file.close();
 
         //Load Header
         const reader = file.reader();
-        const header = try reader.readStruct(Header); //Perhaps there is better error handling
+        const header_ptr = try allocator.create(Header);
+        header_ptr.* = try reader.readStruct(Header);
 
         //Load Data
-        //const c = @as(i32, @intFromFloat(b));
-        const vox_offset = @as(u32, @intFromFloat(header.voxOffset)); //@intFromFloat(u32, @intFromFloat(header.voxOffset));
-
+        const vox_offset = @as(u32, @intFromFloat(header_ptr.voxOffset));
         try file.seekTo(vox_offset);
         const file_size = try file.getEndPos();
-        const allocator = std.heap.page_allocator;
 
         const raw_data = try allocator.alloc(u8, (file_size - vox_offset));
         _ = try file.readAll(raw_data);
 
-        return Image{ .header = &header, .data = &raw_data };
+        return Image{ .header = header_ptr, .data = raw_data, .allocator = allocator };
+    }
+
+    pub fn deinit(self: *const Image) void {
+        self.allocator.destroy(self.header);
+        self.allocator.free(self.data);
     }
 };
 
@@ -87,6 +92,7 @@ test "open" {
     const static = "/Users/joachimpfefferkorn/repos/neurovolume/media/sub-01_T1w.nii";
     const img = try Image.init(static);
     (&img).printHeader();
+    img.deinit();
 }
 
 //convention:
