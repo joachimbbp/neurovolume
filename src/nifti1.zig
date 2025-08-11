146524ee7d8b79ffed91a0f6da00bc40a6e0bd92
joachimbbp <104856283+joachimbbp@users.noreply.github.com>
2025-08-11 14:56:34 -0400

    minmax WIP (seems broken), de-ooping

--- src/nifti1.zig
+++ src/nifti1.zig
@@ -84,7 +84,9 @@ pub const Image = struct {
     data: []const u8,
     allocator: *const std.mem.Allocator,
     data_type: DataType,
-    bytes_per_voxel: i16,
+    bytes_per_voxel: u16,
+
+    //const Self = @This();
 
     pub fn getAt4D(self: *const Image, pos: [4]usize) !f32 {
         const nx: usize = @intCast(self.header.dim[1]);
@@ -108,7 +110,7 @@ pub const Image = struct {
     pub fn printHeader(self: *const Image) void {
         print("{any}", .{self.header});
     }
-    fn byteToFloat(raw_data: []const u8, bytes_per_voxel: i16, bit_start: usize, bit_end: usize) !f32 {
+    fn byteToFloat(raw_data: []const u8, bytes_per_voxel: u16, bit_start: usize, bit_end: usize) !f32 {
         const raw_value = switch (bytes_per_voxel) {
             2 => btf2(raw_data[bit_start..bit_end]),
             //TODO: all of the other byte to float functions!
@@ -117,32 +119,12 @@ pub const Image = struct {
         };
         return raw_value;
     }
-
-    //Byte to float functions
     fn btf2(bytes: []const u8) f32 {
         //This was GPT suggested *but* I checked through the docs and it *seems* correct
         //The reason this casts to f32 is so that it's precision can be expanded
         //with scl slope and inter (in the get at function)
         const value = std.mem.readInt(u16, bytes[0..2], .little);
         return @floatFromInt(value);
-    }
-
-    fn getMinMax(self: *const Image) [2]f32 {
-        var minmax: [2]i128 = .{ std.math.maxInt(i128), std.math.minInt(i128) };
-        for (0..@as(usize, @intCast(self.header.dims[3]))) |z| {
-            for (0..@as(usize, @intCast(self.header.dim[2]))) |x| {
-                for (0..@as(usize, @intCast(self.header.dim[1]))) |y| {
-                    const val = try self.getAt4D([4]usize{ x, y, z, 0 });
-                    if (val < minmax[0]) {
-                        minmax[0] = val;
-                    }
-                    if (val > minmax[1]) {
-                        minmax[1] = val;
-                    }
-                }
-            }
-        }
-        return minmax;
     }
 
     pub fn init(filepath: []const u8) anyerror!Image {
@@ -166,11 +148,20 @@ pub const Image = struct {
 
         //datatype
         const data_type: DataType = @enumFromInt(header_ptr.*.datatype);
-        const bytes_per_voxel = @divTrunc(header_ptr.*.bitpix, 8);
+        const bytes_per_voxel: u16 = @intCast(@divTrunc(header_ptr.*.bitpix, 8));
 
-        return Image{ .header = header_ptr, .data = raw_data, .allocator = allocator, .data_type = data_type, .bytes_per_voxel = bytes_per_voxel };
+        return Image{
+            .header = header_ptr,
+            .data = raw_data,
+            .allocator = allocator,
+            .data_type = data_type,
+            .bytes_per_voxel = bytes_per_voxel,
+        };
     }
-
+    //TODO: You need to prep the image with minmax to get the normalization function!
+    //Some oop-y design habits to destory!
+    //
+    //
     pub fn deinit(self: *const Image) void {
         //TODO: everything else
         //why did robbie put this as destroy for one and free for the other?
@@ -178,10 +169,30 @@ pub const Image = struct {
         self.allocator.free(self.data);
     }
 };
+fn minMax(img: Image) ![2]f32 {
+    var minmax: [2]f32 = .{ std.math.floatMax(f32), std.math.floatMax(f32) };
+    //dim is [num dimensions, x, y, z, time ...]
+    for (0..@as(usize, @intCast(img.header.dim[4]))) |t| {
+        for (0..@as(usize, @intCast(img.header.dim[3]))) |z| {
+            for (0..@as(usize, @intCast(img.header.dim[2]))) |x| {
+                for (0..@as(usize, @intCast(img.header.dim[1]))) |y| {
+                    const val = try img.getAt4D([4]usize{ x, y, z, t });
+                    if (val < minmax[0]) {
+                        minmax[0] = val;
+                    }
+                    if (val > minmax[1]) {
+                        minmax[1] = val;
+                    }
+                }
+            }
+        }
+    }
+    return minmax;
+}
 
-test "open" {
+test "open and normalize" {
     const static = "/Users/joachimpfefferkorn/repos/neurovolume/media/sub-01_T1w.nii";
-    const img = try Image.init(static);
+    var img = try Image.init(static);
     defer img.deinit();
     (&img).printHeader();
     print("\ndatatype: {s}\n", .{DataType.name(img.data_type)});
@@ -194,7 +205,11 @@ test "open" {
 
     const mid_value = try img.getAt4D([4]usize{ mid_x, mid_y, mid_z, mid_t });
 
-    print("middle value: {d}\n", .{mid_value});
+    print("middle value: {}\n", .{mid_value});
+
+    print("Normalizing\n", .{});
+    const minmax = try minMax(img);
+    print("Min Max: {d}\n", .{minmax});
 }
 
 //convention:
