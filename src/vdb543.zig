0b9cb780db687fa73fcde41cfda57664d00edd4a
joachimbbp <104856283+joachimbbp@users.noreply.github.com>
2025-09-05 20:27:54 -0400

    scalar writing functions dried to one

--- src/vdb543.zig
+++ src/vdb543.zig
@@ -13,7 +13,6 @@ fn writePointer(buffer: *ArrayList(u8), pointer: *const u8, len: usize) void {
 }
 fn writeSlice(comptime T: type, buffer: *ArrayList(u8), slice: []const T) void {
     const byte_data = std.mem.sliceAsBytes(slice);
-    //print("writeSlice byte_data: {x}\n", .{byte_data});
     buffer.appendSlice(byte_data) catch unreachable;
 }
 fn writeU8(buffer: *ArrayList(u8), value: u8) void {
@@ -21,45 +20,18 @@ fn writeU8(buffer: *ArrayList(u8), value: u8) void {
 }
 //NOTE:
 //Check endianness for these
-//These could probably be dryed with comptime values!
-//      (I mean look, they're all the same!)
-fn writeU16(buffer: *ArrayList(u8), value: u16) void {
-    buffer.appendSlice(std.mem.asBytes(&value)) catch unreachable;
-    //  writePointer(buffer, &value, @sizeOf(value));
-}
-
-fn writeF32(buffer: *ArrayList(u8), value: f32) void {
+fn writeScalar(comptime T: type, buffer: *ArrayList(u8), value: T) void {
     buffer.appendSlice(std.mem.asBytes(&value)) catch unreachable;
 }
 
-fn writeU32(buffer: *ArrayList(u8), value: u32) void {
-    //NOTE: Need to change this to a const u32 somehow (but efficiently)
-
-    buffer.appendSlice(std.mem.asBytes(&value)) catch unreachable;
-
-    //  writePointer(buffer, &v, @sizeOf(value));
-}
-fn writeU64(buffer: *ArrayList(u8), value: u64) void {
-    //  writePointer(buffer, &value, @sizeOf(value));
-    buffer.appendSlice(std.mem.asBytes(&value)) catch unreachable;
-}
-fn writeU128(buffer: *ArrayList(u8), value: u128) void {
-    //  writePointer(buffer, &value, @sizeOf(value));
-    buffer.appendSlice(std.mem.asBytes(&value)) catch unreachable;
-}
-//NOTE: another good argument for comptiming these: this vdb data value should be arbitrary:
-fn writeF64(buffer: *ArrayList(u8), value: f64) void {
-    //  writePointer(buffer, &value, @sizeOf(value));
-    buffer.appendSlice(std.mem.asBytes(&value)) catch unreachable;
-}
 fn castInt32ToU32(value: i32) u32 {
     const result: u32 = @bitCast(value);
     return result;
 }
 fn writeVec3i(buffer: *ArrayList(u8), value: [3]i32) void {
-    writeU32(buffer, castInt32ToU32(value[0]));
-    writeU32(buffer, castInt32ToU32(value[1]));
-    writeU32(buffer, castInt32ToU32(value[2]));
+    writeScalar(u32, buffer, castInt32ToU32(value[0]));
+    writeScalar(u32, buffer, castInt32ToU32(value[1]));
+    writeScalar(u32, buffer, castInt32ToU32(value[2]));
 }
 
 fn writeString(buffer: *ArrayList(u8), string: []const u8) void {
@@ -68,7 +40,7 @@ fn writeString(buffer: *ArrayList(u8), string: []const u8) void {
     }
 }
 fn writeName(buffer: *ArrayList(u8), name: []const u8) void {
-    writeU32(buffer, @intCast(name.len));
+    writeScalar(u32, buffer, @intCast(name.len));
     writeString(buffer, name);
 }
 fn writeMetaString(buffer: *ArrayList(u8), name: []const u8, string: []const u8) void {
@@ -79,13 +51,13 @@ fn writeMetaString(buffer: *ArrayList(u8), name: []const u8, string: []const u8)
 fn writeMetaBool(buffer: *ArrayList(u8), name: []const u8, value: bool) void {
     writeName(buffer, name);
     writeName(buffer, "bool");
-    writeU32(buffer, 1); //bool is stored in one whole byte
+    writeScalar(u32, buffer, 1); //bool is stored in one whole byte
     writeU8(buffer, if (value) 1 else 0);
 }
 fn writeMetaVector(buffer: *ArrayList(u8), name: []const u8, value: [3]i32) void {
     writeName(buffer, name);
     writeName(buffer, "vec3i");
-    writeU32(buffer, 3 * @sizeOf(i32));
+    writeScalar(u32, buffer, 3 * @sizeOf(i32));
     writeVec3i(buffer, value);
 }
 //VDB nodes
@@ -208,33 +180,33 @@ fn writeNode5Header(buffer: *ArrayList(u8), node: *Node5) void {
     writeVec3i(buffer, .{ 0, 0, 0 });
     //child masks:
     for (node.mask) |child_mask| {
-        writeU64(buffer, child_mask);
+        writeScalar(u64, buffer, child_mask);
     }
     //Presently we don't have any values masks, so just zeroing those out
     for (node.mask) |_| {
-        writeU64(buffer, 0);
+        writeScalar(u64, buffer, 0);
     }
     //Write uncompressed (signified by 6) node values
     writeU8(buffer, 6);
     var i: usize = 0;
     while (i < 32768) : (i += 1) {
-        writeF32(buffer, 0);
+        writeScalar(f32, buffer, 0);
     }
 }
 fn writeNode4Header(buffer: *ArrayList(u8), node: *Node4) void {
     //Child masks
     for (node.mask) |child_mask| {
-        writeU64(buffer, child_mask);
+        writeScalar(u64, buffer, child_mask);
     }
     //No value masks atm
     for (node.mask) |_| {
-        writeU64(buffer, 0);
+        writeScalar(u64, buffer, 0);
     }
 
     writeU8(buffer, 6);
     var i: usize = 0;
     while (i < 4096) : (i += 1) {
-        writeF32(buffer, 0);
+        writeScalar(f32, buffer, 0);
     }
 }
 
@@ -244,10 +216,10 @@ const TreeError = error{
 };
 
 fn writeTree(buffer: *ArrayList(u8), vdb: *VDB) void {
-    writeU32(buffer, 1); //Number of value buffers per leaf node (only change for multi-core implementations)
-    writeF32(buffer, 0); //Root node background value
-    writeU32(buffer, 0); //number of tiles
-    writeU32(buffer, 1); //number of 5 nodes
+    writeScalar(u32, buffer, 1); //Number of value buffers per leaf node (only change for multi-core implementations)
+    writeScalar(u32, buffer, 0); //Root node background value
+    writeScalar(u32, buffer, 0); //number of tiles
+    writeScalar(u32, buffer, 1); //number of 5 nodes
 
     const node_5 = vdb.five_node;
 
@@ -269,7 +241,7 @@ fn writeTree(buffer: *ArrayList(u8), vdb: *VDB) void {
                     const bit_index_3n_t = @as(u32, @intCast(four_mask_idx_t)) * @as(u32, @intCast(64)) + @as(u32, @ctz(four_mask_t));
                     const node_3_t = node_4.three_nodes.get(bit_index_3n_t).?;
                     for (node_3_t.mask) |three_mask| {
-                        writeU64(buffer, three_mask);
+                        writeScalar(u64, buffer, three_mask);
                     }
                 }
             }
@@ -289,7 +261,7 @@ fn writeTree(buffer: *ArrayList(u8), vdb: *VDB) void {
                     const bit_index_3_d = @as(u32, @intCast(four_mask_idx_d)) * 64 + @as(u32, @intCast(@ctz(four_mask_d)));
                     const node_3_d = node_4_t.three_nodes.get(bit_index_3_d).?;
                     for (node_3_d.mask) |three_mask| {
-                        writeU64(buffer, three_mask); //we must re-write the masks for some reason
+                        writeScalar(u64, buffer, three_mask); //we must re-write the masks for some reason
                     }
                     writeU8(buffer, 6); //6 means no compression
                     writeSlice(f32, buffer, &node_3_d.data);
@@ -302,7 +274,7 @@ fn writeTree(buffer: *ArrayList(u8), vdb: *VDB) void {
 
 fn writeMetadata(buffer: *ArrayList(u8)) void {
     // lots of hard coded things that will by dynamic if we expand this!
-    writeU32(buffer, 4); //write number of entries
+    writeScalar(u32, buffer, 4); //write number of entries
     writeMetaString(buffer, "class", "unknown");
     writeMetaString(buffer, "file_compression", "none");
     writeMetaBool(buffer, "is_saved_as_half_float", false);
@@ -312,25 +284,25 @@ fn writeMetadata(buffer: *ArrayList(u8)) void {
 fn writeTransform(buffer: *ArrayList(u8), affine: [4][4]f64) void {
     writeName(buffer, "AffineMap");
 
-    writeF64(buffer, affine[0][0]);
-    writeF64(buffer, affine[1][0]);
-    writeF64(buffer, affine[2][0]);
-    writeF64(buffer, 0);
+    writeScalar(f64, buffer, affine[0][0]);
+    writeScalar(f64, buffer, affine[1][0]);
+    writeScalar(f64, buffer, affine[2][0]);
+    writeScalar(f64, buffer, 0);
 
-    writeF64(buffer, affine[0][1]);
-    writeF64(buffer, affine[1][1]);
-    writeF64(buffer, affine[2][1]);
-    writeF64(buffer, 0);
+    writeScalar(f64, buffer, affine[0][1]);
+    writeScalar(f64, buffer, affine[1][1]);
+    writeScalar(f64, buffer, affine[2][1]);
+    writeScalar(f64, buffer, 0);
 
-    writeF64(buffer, affine[0][2]);
-    writeF64(buffer, affine[1][2]);
-    writeF64(buffer, affine[2][2]);
-    writeF64(buffer, 0);
+    writeScalar(f64, buffer, affine[0][2]);
+    writeScalar(f64, buffer, affine[1][2]);
+    writeScalar(f64, buffer, affine[2][2]);
+    writeScalar(f64, buffer, 0);
 
-    writeF64(buffer, affine[0][3]);
-    writeF64(buffer, affine[1][3]);
-    writeF64(buffer, affine[2][3]);
-    writeF64(buffer, 1);
+    writeScalar(f64, buffer, affine[0][3]);
+    writeScalar(f64, buffer, affine[1][3]);
+    writeScalar(f64, buffer, affine[2][3]);
+    writeScalar(f64, buffer, 1);
 }
 
 fn writeGrid(buffer: *ArrayList(u8), vdb: *VDB, affine: [4][4]f64) void {
@@ -342,15 +314,15 @@ fn writeGrid(buffer: *ArrayList(u8), vdb: *VDB, affine: [4][4]f64) void {
     writeName(buffer, "Tree_float_5_4_3");
 
     //Indicate no instance parent
-    writeU32(buffer, 0);
+    writeScalar(u32, buffer, 0);
 
     //Grid descriptor stream position
-    writeU64(buffer, @as(u64, @intCast(buffer.items.len)) + @sizeOf(u64) * 3);
-    writeU64(buffer, 0);
-    writeU64(buffer, 0);
+    writeScalar(u64, buffer, @as(u64, @intCast(buffer.items.len)) + @sizeOf(u64) * 3);
+    writeScalar(u64, buffer, 0);
+    writeScalar(u64, buffer, 0);
 
     //no compression
-    writeU32(buffer, 0);
+    writeScalar(u32, buffer, 0);
 
     writeMetadata(buffer);
     writeTransform(buffer, affine);
@@ -362,11 +334,11 @@ pub fn writeVDB(buffer: *ArrayList(u8), vdb: *VDB, affine: [4][4]f64) void {
     writeSlice(u8, buffer, &.{ 0x20, 0x42, 0x44, 0x56, 0x0, 0x0, 0x0, 0x0 });
 
     //File Version
-    writeU32(buffer, 224);
+    writeScalar(u32, buffer, 224);
 
     //Library version (pretend OpenVDB 8.1)
-    writeU32(buffer, 8);
-    writeU32(buffer, 1);
+    writeScalar(u32, buffer, 8);
+    writeScalar(u32, buffer, 1);
 
     //no grid offsets
     writeU8(buffer, 0);
@@ -376,16 +348,16 @@ pub fn writeVDB(buffer: *ArrayList(u8), vdb: *VDB, affine: [4][4]f64) void {
     writeString(buffer, "7a0f79c6-c47a-4954-8af8-8a9dcc384448");
 
     //No Metadata for now
-    writeU32(buffer, 0);
+    writeScalar(u32, buffer, 0);
 
     //One Grid
-    writeU32(buffer, 1);
+    writeScalar(u32, buffer, 1);
 
     writeGrid(buffer, vdb, affine);
 }
 
 // Utility functions
-// Move these?
+//TODO: Move these?
 pub fn toF32(v: [3]usize) [3]f32 {
     return .{
         @floatFromInt(v[0]),
