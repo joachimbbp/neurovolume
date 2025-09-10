a43a764e3b666148dfb3664c3880c7064b60ea52
joachimbbp <104856283+joachimbbp@users.noreply.github.com>
2025-09-10 18:21:58 -0400

    NIFTI WORKS! I just had the wrong paths before (tired today)

--- python/import_test.py
+++ python/import_test.py
@@ -1,10 +1,10 @@
 from ctypes import cdll
 
-lib_path = b"/Users/joachimpfefferkorn/repos/neurovolume/zig-out/lib/libneurovolume.dylib"
+lib_path = "/Users/joachimpfefferkorn/repos/neurovolume/zig-out/lib/libneurovolume.dylib"
 
 lib = cdll.LoadLibrary(lib_path)
-
+nifti_path = b"/Users/joachimpfefferkorn/repos/neurovolume/media/sub-01_T1w.nii"
 lib.hello()
 echo_me = b"ham spam land echo\n"
 lib.echo(echo_me)
-lib.nifti1ToVDB(lib_path, True)
+lib.nifti1ToVDB(nifti_path, True)
