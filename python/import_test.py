425afcaf9dc48ac42bed34f366e6951c58a95574
joachimbbp <104856283+joachimbbp@users.noreply.github.com>
2025-09-11 10:56:45 -0400

    echo works

--- python/import_test.py
+++ python/import_test.py
@@ -1,10 +1,16 @@
-from ctypes import cdll
+from ctypes import cdll, c_char_p
 
 lib_path = "/Users/joachimpfefferkorn/repos/neurovolume/zig-out/lib/libneurovolume.dylib"
 
+# NOTE: you could do simple wrappers that abstract away the 'b' infront of the strings
 lib = cdll.LoadLibrary(lib_path)
-nifti_path = b"/Users/joachimpfefferkorn/repos/neurovolume/media/sub-01_T1w.nii"
+lib.echo.argtypes = [c_char_p]
+# nifti_path = b"/Users/joachimpfefferkorn/repos/neurovolume/media/sub-01_T1w.nii"
+lib.echo.restype = c_char_p
+# lib.hello()
 lib.hello()
-echo_me = b"ham spam land echo\n"
-lib.echo(echo_me)
-lib.nifti1ToVDB(nifti_path, True)
+echo_me = b"ham spam land echo\0"
+print(lib.echo(echo_me))
+result = lib.echo(echo_me)
+print(f"echo result: {result.decode('utf-8')}")
+# lib.nifti1ToVDB(nifti_path, True)
