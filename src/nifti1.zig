5fb665caafeb4e9ccef23ec4ac9d9cc445e35de5
joachimbbp <104856283+joachimbbp@users.noreply.github.com>
2025-10-27 17:44:56 -0400

    checkpoint

--- src/nifti1.zig
+++ src/nifti1.zig
@@ -53,6 +53,10 @@ pub const Header = extern struct {
     intentName: [16]u8,
 
     magic: [4]u8,
+
+    //WARN:
+    //Let's see if it's possible to put a function here?
+
 };
 //Maybe move to root? follow same pattern as measurementUnits_c
 pub const DataType = enum(i16) {
@@ -76,109 +80,107 @@ pub const DataType = enum(i16) {
     long_double_pair = 2048,
     rgba = 2304,
     //anyhting else is unknown
-    pub fn name(field: DataType) [:0]const u8 {
+    fn name(field: DataType) [:0]const u8 {
         return @tagName(field);
     }
-};
-
-//DEPRECATED: Image should be universal to all input formats, not just nifti 1
-//header extraction has to become it's own thing too, so there's lots of
-//stuff that will neeed DRYing in the future
-pub const Image = struct {
-    header: *const Header,
-    data: []const u8,
-    allocator: *const std.mem.Allocator,
-    data_type: DataType,
-    bytes_per_voxel: u16,
-
-    pub fn getAt4D(self: *const Image, xpos: usize, ypos: usize, zpos: usize, tpos: usize, normalize: bool, minmax: [2]f32) !f32 {
-        const nx: usize = @intCast(self.header.dim[1]);
-        const ny: usize = @intCast(self.header.dim[2]);
-        const nz: usize = @intCast(self.header.dim[3]);
-
-        const idx: usize = tpos * nx * ny * nz + zpos * nx * ny + ypos * nx + xpos;
-
-        const bit_start: usize = idx * @as(usize, @intCast(self.bytes_per_voxel));
-        const bit_end: usize = (idx + 1) * @as(usize, @intCast(self.bytes_per_voxel));
-
-        const raw_value = try byteToFloat(self.data, self.bytes_per_voxel, bit_start, bit_end);
-
-        var post_slope = raw_value;
-        if (self.header.sclSlope != 0) {
-            post_slope = self.header.sclSlope * raw_value + self.header.sclInter;
-        }
-
-        if (normalize) {
-            return (post_slope - minmax[0]) / (minmax[1] - minmax[0]); //TODO: calc the denom only once
-        } else {
-            return post_slope;
-        }
-    }
-
-    pub fn printHeader(self: *const Image) void {
-        print("🧠 Nifti Header:\n-------- \n{any}\n--------\n", .{self.header});
-    }
-
-    fn byteToFloat(raw_data: []const u8, bytes_per_voxel: u16, bit_start: usize, bit_end: usize) !f32 {
-        const raw_value = switch (bytes_per_voxel) {
-            2 => btf2(raw_data[bit_start..bit_end]),
-            //TODO: all of the other byte to float functions!
-            //which migth requrie multiple types?
-            else => return AccessError.NotSupportedYet,
-        };
-        return raw_value;
-    }
-
-    fn btf2(bytes: []const u8) f32 {
-        const value = std.mem.readInt(i16, bytes[0..2], .little); // i16, not u16
-        return @floatFromInt(value);
-    }
-    //DEPRECATED: This can be made much better
-    pub fn init(filepath: []const u8) anyerror!Image {
-        const allocator = &std.heap.page_allocator;
-        //Load File
-        const file = try std.fs.cwd().openFile(filepath, .{});
-        defer file.close();
-
-        //Load Header
-        const reader = std.fs.File.deprecatedReader(file);
-
-        const header_ptr = try allocator.create(Header);
-        header_ptr.* = try reader.readStruct(Header);
-
-        //Load Data
-        const vox_offset = @as(u32, @intFromFloat(header_ptr.voxOffset));
-        try file.seekTo(vox_offset);
-        const file_size = try file.getEndPos();
-
-        const raw_data = try allocator.alloc(u8, (file_size - vox_offset));
-        _ = try file.readAll(raw_data);
-
-        //datatype
-        const data_type: DataType = @enumFromInt(header_ptr.*.datatype);
-        const bytes_per_voxel: u16 = @intCast(@divTrunc(header_ptr.*.bitpix, 8));
-
-        return Image{
-            .header = header_ptr,
-            .data = raw_data,
-            .allocator = allocator,
-            .data_type = data_type,
-            .bytes_per_voxel = bytes_per_voxel,
-        };
-    }
-    //TODO: You need to prep the image with minmax to get the normalization function!
-    //Some oop-y design habits to destory!
-    //
-    //
-    pub fn deinit(self: *const Image) void {
-        //TODO: everything else
-        //why did robbie put this as destroy for one and free for the other?
-        self.allocator.destroy(self.header);
-        self.allocator.free(self.data);
+    pub fn get(hdr_ptr: *Header) !DataType {
+        const data_type_tag: DataType = @enumFromInt(hdr_ptr.*.datatype);
+        return DataType.name(data_type_tag);
     }
 };
 
-pub fn getHeader(filepath: []const u8) !*const Header {
+pub fn loadHeaderPointer(filepath: []const u8, alloc: std.mem.Allocator) !*Header {
+    const file = try std.fs.cwd().openFile(filepath, .{});
+    defer file.close();
+    const reader = std.fs.File.deprecatedReader(file);
+    const header_ptr = try alloc.create(Header);
+    header_ptr.* = try reader.readStruct(Header);
+    return header_ptr;
+}
+
+pub fn getRawData(filepath: []const u8) ![]u8 {
+    const alloc = &std.heap.page_allocator; //WARN: why page_allocator?
+
+    const file = try std.fs.cwd().openFile(filepath, .{});
+    defer file.close();
+    const hdr_ptr = loadHeaderPointer(filepath, alloc);
+    const vox_offset = @as(u32, @intFromFloat(hdr_ptr.voxOffset));
+    try file.seekTo(vox_offset);
+    const file_size = try file.getEndPos();
+
+    const raw_data = try alloc.alloc(u8, (file_size - vox_offset)); //wow, CURSED: naming!
+    try file.readAll(raw_data);
+    return raw_data;
+}
+
+pub fn getAt4D(
+    xpos: usize,
+    ypos: usize,
+    zpos: usize,
+    tpos: usize,
+    data: []u8,
+    header_pointer: Header,
+    normalize: bool,
+    minmax: [2]f32,
+) !f32 {
+    const nx: usize = @intCast(header_pointer.*.dim[1]);
+    const ny: usize = @intCast(header_pointer.*.dim[2]);
+    const nz: usize = @intCast(header_pointer.*.dim[3]);
+
+    const idx: usize = tpos * nx * ny * nz + zpos * nx * ny + ypos * nx + xpos;
+
+    const bytes_per_voxel: u16 = @intCast(@divTrunc(header_pointer.*.bitpix, 8));
+    const bit_start: usize = idx * @as(usize, @intCast(bytes_per_voxel));
+    const bit_end: usize = (idx + 1) * @as(usize, @intCast(bytes_per_voxel));
+
+    const raw_value = try byteToFloat(
+        data,
+        bytes_per_voxel,
+        bit_start,
+        bit_end,
+    );
+
+    //OPTIMIZE: not the most performant at the moment, but I'm going to replace
+    //the whole shebang with Jan's get_at logic, so I'm not going to focus
+    //on optimizing this temporary code
+    var post_slope = raw_value;
+    if (header_pointer.*.sclSlope != 0) {
+        post_slope = header_pointer.*.sclSlope * raw_value + header_pointer.*.sclInter;
+    }
+
+    if (normalize) {
+        return (post_slope - minmax[0]) / (minmax[1] - minmax[0]);
+    } else {
+        return post_slope;
+    }
+}
+
+pub fn printHeader(hdr_ptr: *Header) void {
+    //TODO: this could use some custom parsing etc
+    print("🧠 Nifti Header:\n-------- \n{any}\n--------\n", .{hdr_ptr});
+}
+
+fn byteToFloat(raw_data: []const u8, bytes_per_voxel: u16, bit_start: usize, bit_end: usize) !f32 {
+    const raw_value = switch (bytes_per_voxel) {
+        2 => btf2(raw_data[bit_start..bit_end]),
+        //TODO: all of the other byte to float functions!
+        //which migth requrie multiple types?
+        else => return AccessError.NotSupportedYet,
+    };
+    return raw_value;
+}
+
+fn btf2(bytes: []const u8) f32 {
+    const value = std.mem.readInt(i16, bytes[0..2], .little); // i16, not u16
+    return @floatFromInt(value);
+}
+
+pub fn getBytesPerVoxel(hdr_ptr: *Header) !u16 {
+    const bytes_per_voxel: u16 = @intCast(@divTrunc(hdr_ptr.*.bitpix, 8));
+    return bytes_per_voxel;
+}
+
+pub fn getHeader(filepath: []const u8) !*Header {
     const allocator = &std.heap.page_allocator;
     //Load File
     const file = try std.fs.cwd().openFile(filepath, .{});
@@ -210,13 +212,13 @@ pub fn getTransform(h: Header) ![4][4]f64 {
     };
 }
 
-pub fn MinMax3D(img: Image) ![2]f32 {
+pub fn getMinMax3D(dim: [4]usize) ![2]f32 {
+    //OPTIMIZE: this could probably mimic Jan's eventual logic in getAt4D
     var minmax: [2]f32 = .{ std.math.floatMax(f32), -std.math.floatMax(f32) };
-    //dim is [num dimensions, x, y, z, time ...]
-    for (0..@as(usize, @intCast(img.header.dim[3]))) |z| {
-        for (0..@as(usize, @intCast(img.header.dim[2]))) |y| {
-            for (0..@as(usize, @intCast(img.header.dim[1]))) |x| {
-                const val = try img.getAt4D(x, y, z, 0, false, .{ 0, 0 });
+    for (0..@as(usize, @intCast(dim[3]))) |z| {
+        for (0..@as(usize, @intCast(dim[2]))) |y| {
+            for (0..@as(usize, @intCast(dim[1]))) |x| {
+                const val = try getAt4D(x, y, z, 0, false, .{ 0, 0 });
                 if (val < minmax[0]) {
                     minmax[0] = val;
                 }
@@ -238,25 +240,42 @@ test "echo module" {
 test "open and normalize nifti file" {
     print("🧠 Opening and normalizing nifti1 file\n", .{});
     const static = config.testing.files.nifti1_t1;
-    var img = try Image.init(static);
-    defer img.deinit();
-    (&img).printHeader();
-    print("\ndatatype: {s}\n", .{DataType.name(img.data_type)});
-    print("bytes per voxel: {any}\n", .{img.bytes_per_voxel});
+    const hdr_ptr = try getHeader(static);
+    printHeader(hdr_ptr);
+    print("\ndatatype: {s}\n", .{DataType.get(hdr_ptr)});
+    const bpv = try getBytesPerVoxel(hdr_ptr);
+    print("bytes per voxel: {any}\n", .{bpv});
 
-    const mid_x: usize = @divFloor(@as(usize, @intCast(img.header.dim[1])), 2);
-    const mid_y: usize = @divFloor(@as(usize, @intCast(img.header.dim[2])), 2);
-    const mid_z: usize = @divFloor(@as(usize, @intCast(img.header.dim[3])), 2);
-    const mid_t: usize = @divFloor(@as(usize, @intCast(img.header.dim[4])), 2);
+    const mid_x: usize = @divFloor(@as(usize, @intCast(hdr_ptr.*.img.header.dim[1])), 2);
+    const mid_y: usize = @divFloor(@as(usize, @intCast(hdr_ptr.*.img.header.dim[2])), 2);
+    const mid_z: usize = @divFloor(@as(usize, @intCast(hdr_ptr.*.dim[3])), 2);
+    const mid_t: usize = @divFloor(@as(usize, @intCast(hdr_ptr.*.dim[4])), 2);
+    const minmax = try getMinMax3D(mid_x, mid_y, mid_z, mid_t);
+    const raw_data = try getRawData(static);
 
-    const mid_value = try img.getAt4D(mid_x, mid_y, mid_z, mid_t, false, .{ 0, 0 });
+    const mid_value = try getAt4D(
+        mid_x,
+        mid_y,
+        mid_z,
+        mid_t,
+        raw_data,
+        hdr_ptr,
+        true,
+        minmax,
+    );
 
     print("middle value: {any}\n", .{mid_value});
 
     print("Normalizing\nSetting Min Max\n", .{});
-    const minmax = try MinMax3D(img);
     print("Min Max: {any}\n", .{minmax});
-    const normalized_mid_value = try img.getAt4D(mid_x, mid_y, mid_z, mid_t, true, minmax);
+    const normalized_mid_value = try getAt4D(
+        mid_x,
+        mid_y,
+        mid_z,
+        mid_t,
+        true,
+        minmax,
+    );
     print("Normalized mid value: {any}\n", .{normalized_mid_value});
 }
 
