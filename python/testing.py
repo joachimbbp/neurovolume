9b4325a87225cf806e6af1490c3c09bdef98719d
joachimbbp <104856283+joachimbbp@users.noreply.github.com>
2025-11-10 14:01:19 -0500

    timing and testing improved

--- python/testing.py
+++ python/testing.py
@@ -1,10 +1,18 @@
 import numpy as np
 import neurovolume_lib as nv
+from datetime import datetime
 
 static_testfile = "/Users/joachimpfefferkorn/repos/neurovolume/media/sub-01_T1w.nii"
 fmri_testfile = "/Users/joachimpfefferkorn/repos/neurovolume/media/sub-01_task-emotionalfaces_run-1_bold.nii"
 
+print("Python tests starting")
+start = datetime.now()
 t1_save_location = nv.nifti1_to_VDB(static_testfile, True)
+end = datetime.now()
+elapsed = end - start
+print(
+    f" nifti1_to_VDB time Elapsed: {elapsed.seconds}s {elapsed.microseconds}µs")
+
 t1_nf = nv.num_frames(static_testfile, "NIfTI1")
 print("🐍 static VDB saved to: ", t1_save_location,
       " with ", t1_nf, " frames\n")
